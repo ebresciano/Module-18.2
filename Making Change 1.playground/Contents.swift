39c4702dac4972e23949952ca7ae0c0731adb662
Eva <Bresciano>
2016-06-06 09:24:28 -0600

    h

---
+++ Making Change 1.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func makeChange(change: Double) -> String {
+    
+    let centsChange = Int(change * 100)
+    let dollars = centsChange / 100
+    let quarters = (centsChange - dollars * 100) / 25
+    let dimes = (centsChange - dollars * 100 - quarters * 25) / 10
+    let nickels = (centsChange - dollars * 100 - quarters * 25 - dimes * 10) / 5
+    let pennies = (centsChange - dollars * 100 - quarters * 25 - dimes * 10 - nickels * 5) / 1
+    
+    return "Your change is \(dollars) dollars, \(quarters) quarters, \(dimes) dimes, \(nickels) nickels, and \(pennies) pennies."
+    
+}
+
+print(makeChange(15.5))
